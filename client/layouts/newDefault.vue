fe1549e6efc2fe07edb6291be1b0da0ffc713a0c
Michael Dunwoody <mic_ael202@hotmail.co.uk>
2021-10-15 16:40:12 +0100

    Logo obtained, first updates to landing page

---
+++ client/layouts/newDefault.vue
@@ -0,0 +1,115 @@
+<template>
+  <div>
+      <section class="header">
+        <div class="custom-shape-divider-top-1634301248">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+            </svg>
+        </div>
+      </section>
+    <Nuxt />
+    <section class="footer">
+        <div class="custom-shape-divider-bottom-1634301354">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+            </svg>
+        </div>
+    </section>
+  </div>
+</template>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+
+/*Delete all CSS above here? */
+.custom-shape-divider-top-1634301248 {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    overflow: hidden;
+    line-height: 0;
+}
+
+.custom-shape-divider-top-1634301248 svg {
+    position: relative;
+    display: block;
+    width: calc(148% + 1.3px);
+    height: 157px;
+}
+
+.custom-shape-divider-top-1634301248 .shape-fill {
+    fill: #2D9FA0;
+}
+
+.custom-shape-divider-bottom-1634301354 {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    overflow: hidden;
+    line-height: 0;
+    transform: rotate(180deg);
+}
+
+.custom-shape-divider-bottom-1634301354 svg {
+    position: relative;
+    display: block;
+    width: calc(148% + 1.3px);
+    height: 157px;
+}
+
+.custom-shape-divider-bottom-1634301354 .shape-fill {
+    fill: #091C58;
+}
+
+.footer .header {
+    position: relative;
+}
+</style>
--- client/pages/login.vue
+++ client/pages/login.vue
@@ -26,9 +26,7 @@
 </template>
 
 <script>
-export default {
-  layout: 'newDefault'
-}
+export default {}
 </script>
 
 <style>
