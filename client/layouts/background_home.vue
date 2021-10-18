32b51c301fc921676329c9f106b7d9c4ced29a1a
Michael Dunwoody <mic_ael202@hotmail.co.uk>
2021-10-19 00:27:41 +0100

    Dashboard time

---
+++ client/layouts/background_home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <section class="home-header"/>
+        <Nuxt />
+    <section class="home-footer"/>
+  </div>
+</template>
+
+<style>
+.home-header, .home-footer{
+    width: 100%;
+    position: fixed;        
+    padding: 10px 0;
+    color: #ffff;
+}
+.home-header{
+    top: 0;
+    background: #091C58;
+}
+.home-footer{
+    bottom: 0;
+    background: #2D9FA0;
+}   
+</style>
