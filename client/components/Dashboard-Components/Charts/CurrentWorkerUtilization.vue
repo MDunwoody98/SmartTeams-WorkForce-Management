878056bfe8195927f76ba2a528f8be2e371d0cfd
Michael Dunwoody <mic_ael202@hotmail.co.uk>
2021-12-03 13:23:39 +0000

    Issues

---
+++ client/components/Dashboard-Components/Charts/CurrentWorkerUtilization.vue
@@ -0,0 +1,29 @@
+<script>
+import { Pie } from 'vue-chartjs'
+
+export default {
+  extends: Pie,
+  props: {
+    chartData: {
+      type: Object,
+      default: null,
+    },
+    options: {
+      type: Object,
+      default: null,
+    },
+  },
+  mounted() {
+    this.renderChart({
+      labels: ['Absence', 'Non-utilized', 'Utilized'],
+      datasets: [
+        {
+          label: 'Annual Leave',
+          backgroundColor: ['grey', '#091C58', '#2D9FA0'],
+          data: [4, 20, 76],
+        },
+      ],
+    })
+  },
+}
+</script>
---
+++ client/components/Dashboard-Components/Charts/RemainingLeave.vue
@@ -0,0 +1,29 @@
+<script>
+import { Pie } from 'vue-chartjs'
+
+export default {
+  extends: Pie,
+  props: {
+    chartData: {
+      type: Object,
+      default: null,
+    },
+    options: {
+      type: Object,
+      default: null,
+    },
+  },
+  mounted() {
+    this.renderChart({
+      labels: ['Leave used', 'Leave remaining'],
+      datasets: [
+        {
+          label: 'Annual Leave',
+          backgroundColor: ['#091C58', '#2D9FA0'],
+          data: [16.25, 35],
+        },
+      ],
+    })
+  },
+}
+</script>
