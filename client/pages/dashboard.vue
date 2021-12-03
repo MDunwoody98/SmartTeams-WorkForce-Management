8b818837625418898f438aec2d4d4f0148f13bc4
Michael Dunwoody <mic_ael202@hotmail.co.uk>
2021-12-03 13:43:01 +0000

    Everything fixed. restored oauth module

--- client/pages/dashboard.vue
+++ client/pages/dashboard.vue
@@ -1,7 +1,7 @@
 <template>
   <!--If no worker profile, navigate to a form to complete profile-->
   <div class="container">
-    <v-container v-if="!mobile" class="d-flex justify-center">
+    <v-container v-if="!mobile">
       <v-row>
         <!--Here is where the date/week selector would go-->
         <v-col cols="12">
@@ -10,15 +10,32 @@
         </v-col>
         <v-col cols="6">
           <!--In here goes the time entry cards-->
-          <RemainingLeave />
+          <div class="report-container">
+            <v-card elevation="24" width="97%" class="report-item-card">
+              <v-card-title class="report-item-title"
+                >My Annual Leave</v-card-title
+              >
+              <Charts-RemainingLeave
+                :data="annualLeaveChartData"
+                :options="annualLeaveOptions"
+                class="pie-chart"
+              />
+            </v-card>
+          </div>
         </v-col>
         <v-col cols="6">
           <!--In here goes the vertical split with the graphs-->
-          <div>
-            <Logo />
-            <p>
-              {{ this.$auth.user }}
-            </p>
+          <div class="report-container">
+            <v-card elevation="24" width="97%" class="report-item-card">
+              <v-card-title class="report-item-title"
+                >My Utilization</v-card-title
+              >
+              <Charts-CurrentWorkerUtilization
+                :data="utilizationChartData"
+                :options="utilizationOptions"
+                class="pie-chart"
+              />
+            </v-card>
           </div>
         </v-col>
       </v-row>
@@ -29,6 +46,14 @@
 <script>
 export default {
   layout: 'background_home',
+  data() {
+    return {
+      utilizationChartData: null,
+      utilizationOptions: {},
+      annualLeaveChartData: null,
+      annualLeaveOptions: {},
+    }
+  },
   computed: {
     mobile() {
       this.$auth.refreshTokens()
@@ -41,10 +66,22 @@ export default {
 <style>
 .container {
   margin: 0 auto;
-  min-height: 100vh;
+  height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
+}
+.report-container {
+  display: flex;
+  align-items: stretch;
+  justify-content: space-evenly;
+}
+.report-item-title {
+  justify-content: center;
+}
+.pie-chart {
+  width: 50%;
+  margin: 0 auto;
 }
 </style>
